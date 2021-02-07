30d6d756b2c4d11d08f2e3003d01be5975bfb52d
subhash.naval@gmail.com <subhash.naval@gmail.com>
2021-02-07 13:10:02 +0530

    bird1 added

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird1 = new Bird(100,100);
 
 }
 
@@ -59,6 +59,6 @@ function draw(){
     log4.display();
     log5.display();
 
-    bird.display();
+    bird1.display();
     platform.display();
 }
